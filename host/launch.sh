a76e07300c0e068ec31f97cd9e6d1632ef43bc3a
Randy Sab-Roy <randy.sab.roy@gmail.com>
2024-02-14 13:16:23 -0500

    Loop improvements (#22)
    ## 📖 Description
    
    This PR improves the run loop by checking for changes in the `.env`
    which allows us to change stuff on the fly.
    
    - The `.env` is reloaded at the beginning of the loop
    - Before starting the runner, the script will now validate that the
    image exist, and attempt to pull it if it doesn't.
    - The script will check for a `SCHEDULE_SHUTDOWN` flag in the `.env` and
    will shutdown if it is set to true. This allows us to turn off the
    runner when it has completed its current job.
    - `SIGTERM` events will now be intercepted by the script to allow the
    cleanup to work when it is running as a service

--- host/launch.sh
+++ host/launch.sh
@@ -16,6 +16,8 @@ REGISTRY_IMAGE_NAME=runner
 
 LOGFILE=runner.log
 
+SCHEDULE_SHUTDOWN=false
+
 function log_output {
 	echo "$(date "+%Y/%m/%d %H:%M:%S") $1"
 	echo "$(date "+%Y/%m/%d %H:%M:%S") [${RUN_ID:-PREPARING}] $1" >>$LOGFILE
@@ -27,27 +29,24 @@ function stream_output {
 	done
 }
 
-# Load .env file
-if [ -f .env ]; then
-	# shellcheck disable=SC2046
-	export $(xargs <.env)
-fi
+function reload_env {
+	if [ -f .env ]; then
+		# shellcheck disable=SC2046
+		export $(xargs <.env)
+	fi
+}
 
-# Configure Homebrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
+function cleanup {
+	log_output "[HOST] 🚦 Stopping runner script"
+	exit 0
+}
 
-# Show a shutdown message when closing the script
-trap "log_output \"[HOST] 🚦 Stopping runner script\"; exit 1" SIGINT
+function pull_image {
+	log_output "[HOST] ⬇️ Downloading from remote registry"
+	TART_REGISTRY_USERNAME=$REGISTRY_USERNAME TART_REGISTRY_PASSWORD=$REGISTRY_PASSWORD tart pull "$REGISTRY_PATH"
+}
 
-# Select image
-if [ -n "${REGISTRY_URL}" ]; then
-	REGISTRY_PATH="$REGISTRY_URL/$REGISTRY_IMAGE_NAME"
-else
-	REGISTRY_PATH="$REGISTRY_IMAGE_NAME"
-fi
-
-# Main loop
-while :; do
+function run_loop {
 	RUN_ID="$RANDOM$RANDOM"
 
 	log_output "[HOST] 🎫 Creating registration token"
@@ -55,8 +54,8 @@ while :; do
 
 	log_output "[HOST] 💻 Launching macOS VM"
 	INSTANCE_NAME=runner_"$RUNNER_NAME"_"$RUN_ID"
-	TART_REGISTRY_USERNAME=$REGISTRY_USERNAME TART_REGISTRY_PASSWORD=$REGISTRY_PASSWORD tart clone $REGISTRY_PATH $INSTANCE_NAME
-	trap 'log_output "[HOST] 🪓 Killing the VM"; tart delete $INSTANCE_NAME; log_output "[HOST] 🚦 Stopping runner script"; exit 1' SIGINT
+	tart clone "$REGISTRY_PATH" "$INSTANCE_NAME"
+	trap 'log_output "[HOST] 🪓 Killing the VM"; tart delete $INSTANCE_NAME; cleanup' SIGINT SIGTERM
 	tart run --no-graphics $INSTANCE_NAME >/dev/null 2>&1 &
 
 	log_output "[HOST] 💤 Waiting for VM to boot"
@@ -72,10 +71,10 @@ while :; do
 	done
 
 	log_output "[HOST] 🔨 Configuring runner on VM"
-	ssh -q "$VM_USERNAME@$IP_ADDRESS" "./actions-runner/config.sh --url $RUNNER_URL --token $REGISTRATION_TOKEN --ephemeral --name $RUNNER_NAME --labels $RUNNER_LABELS --unattended --replace" >/dev/null
+	ssh -q -o StrictHostKeyChecking=no "$VM_USERNAME@$IP_ADDRESS" "./actions-runner/config.sh --url $RUNNER_URL --token $REGISTRATION_TOKEN --ephemeral --name $RUNNER_NAME --labels $RUNNER_LABELS --unattended --replace" >/dev/null
 
 	log_output "[HOST] 🏃 Starting runner on VM"
-	ssh -q "$VM_USERNAME@$IP_ADDRESS" "source ~/.zprofile && ./actions-runner/run.sh" 2>&1 | sed -nru 's/^(.+)$/[GUEST] 📀 \1/p' | stream_output
+	ssh -q -o StrictHostKeyChecking=no "$VM_USERNAME@$IP_ADDRESS" "source ~/.zprofile && ./actions-runner/run.sh" 2>&1 | sed -nru 's/^(.+)$/[GUEST] 📀 \1/p' | stream_output
 
 	log_output "[HOST] ✋ Stop the VM"
 	tart stop "$INSTANCE_NAME"
@@ -84,5 +83,39 @@ while :; do
 	tart delete "$INSTANCE_NAME"
 
 	RUN_ID=""
-	trap 'log_output "[HOST] 🚦 Stopping runner script"; exit 1' SIGINT
+	trap cleanup SIGINT SIGTERM
+}
+
+# Configure Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Show a shutdown message when closing the script
+trap cleanup SIGINT SIGTERM
+
+# Main loop
+while :; do
+	reload_env
+
+	if [[ "$SCHEDULE_SHUTDOWN" == "true" ]]; then
+		log_output "[HOST] ⏰ Scheduled for shutdown"
+		cleanup
+	fi
+
+	# Select image
+	if [ -n "${REGISTRY_URL}" ]; then
+		REGISTRY_PATH="$REGISTRY_URL/$REGISTRY_IMAGE_NAME"
+	else
+		REGISTRY_PATH="$REGISTRY_IMAGE_NAME"
+	fi
+
+	if tart list | grep $REGISTRY_PATH; then
+		run_loop
+	else
+		log_output "[HOST] 🔎 Target image not found"
+		if [ -n "${REGISTRY_URL}" ]; then
+			pull_image
+		else
+			cleanup
+		fi
+	fi
 done
